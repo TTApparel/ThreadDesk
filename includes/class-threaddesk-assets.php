628dd99aa3c533ffa60fef9bcbbcc524228f0f60
TTApparel <petros.maniatis99@gmail.com>
2026-02-06 18:09:38 -0500

    Adjust profile header spacing

---
+++ includes/class-threaddesk-assets.php
@@ -0,0 +1,50 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class TTA_ThreadDesk_Assets {
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+	}
+
+	public function enqueue_assets() {
+		if ( $this->is_shortcode_present() ) {
+			$this->enqueue_core_assets();
+			return;
+		}
+
+		if ( ! function_exists( 'is_account_page' ) || ! is_account_page() ) {
+			return;
+		}
+
+		$endpoint = get_query_var( 'thread-desk', false );
+		$section  = get_query_var( 'td_section', '' );
+
+		if ( false === $endpoint && empty( $section ) ) {
+			return;
+		}
+
+		$this->enqueue_core_assets();
+	}
+
+	private function enqueue_core_assets() {
+		wp_enqueue_style( 'threaddesk', THREDDESK_URL . 'assets/css/threaddesk.css', array(), THREDDESK_VERSION );
+		wp_enqueue_script( 'threaddesk', THREDDESK_URL . 'assets/js/threaddesk.js', array( 'jquery' ), THREDDESK_VERSION, true );
+	}
+
+	private function is_shortcode_present() {
+		if ( ! is_singular() ) {
+			return false;
+		}
+
+		global $post;
+
+		if ( ! $post instanceof WP_Post ) {
+			return false;
+		}
+
+		return has_shortcode( $post->post_content, 'threaddesk' );
+	}
+}
