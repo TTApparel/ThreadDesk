2fcbcc34c83f512f29ccd7716e4ad4a096bce21d
TTApparel <petros.maniatis99@gmail.com>
2026-02-09 15:56:42 -0500

    Fix address display and modal script

--- assets/js/threaddesk.js
+++ assets/js/threaddesk.js
@@ -53,4 +53,49 @@ jQuery(function ($) {
 			openModal(defaultPanel);
 		}
 	}
+
+	const addressModal = $('.threaddesk-address-modal');
+
+	if (addressModal.length) {
+		const openAddressModal = function (target) {
+			addressModal.addClass('is-active').attr('aria-hidden', 'false');
+			switchAddressPanel(target);
+		};
+
+		const closeAddressModal = function () {
+			addressModal.removeClass('is-active').attr('aria-hidden', 'true');
+		};
+
+		const switchAddressPanel = function (target) {
+			const panel = target || 'billing';
+			const panels = addressModal.find('[data-threaddesk-address-panel]');
+			const tabs = addressModal.find('[data-threaddesk-address-tab]');
+
+			panels.removeClass('is-active').attr('aria-hidden', 'true');
+			panels.filter('[data-threaddesk-address-panel="' + panel + '"]').addClass('is-active').attr('aria-hidden', 'false');
+
+			tabs.removeClass('is-active').attr('aria-selected', 'false');
+			tabs.filter('[data-threaddesk-address-tab="' + panel + '"]').addClass('is-active').attr('aria-selected', 'true');
+		};
+
+		$(document).on('click', '[data-threaddesk-address]', function (event) {
+			event.preventDefault();
+			const target = $(this).data('threaddesk-address');
+			openAddressModal(target);
+		});
+
+		$(document).on('click', '[data-threaddesk-address-tab]', function () {
+			switchAddressPanel($(this).data('threaddesk-address-tab'));
+		});
+
+		$(document).on('click', '[data-threaddesk-address-close]', function () {
+			closeAddressModal();
+		});
+
+		$(document).on('keyup', function (event) {
+			if (event.key === 'Escape') {
+				closeAddressModal();
+			}
+		});
+	}
 });
