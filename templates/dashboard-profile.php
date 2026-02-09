afddce129b948058cab5fb484bbf1d15f6c90f3b
TTApparel <petros.maniatis99@gmail.com>
2026-02-09 15:31:27 -0500

    Center profile details and improve map fallback

--- templates/dashboard-profile.php
+++ templates/dashboard-profile.php
@@ -11,6 +11,36 @@ $client_name = ! empty( $context['client_name'] ) ? $context['client_name'] : __
 $avatar_url  = ! empty( $context['avatar_url'] ) ? $context['avatar_url'] : '';
 $stats   = isset( $context['order_stats'] ) ? $context['order_stats'] : array();
 $currency = isset( $context['currency'] ) ? $context['currency'] : 'USD';
+$shipping_address = ! empty( $context['shipping_address'] ) && is_array( $context['shipping_address'] ) ? $context['shipping_address'] : array();
+$billing_address  = ! empty( $context['billing_address'] ) && is_array( $context['billing_address'] ) ? $context['billing_address'] : array();
+$address_source   = $shipping_address ? $shipping_address : $billing_address;
+$billing_display  = ! empty( $billing_address['formatted'] ) ? $billing_address['formatted'] : __( 'Not provided yet.', 'threaddesk' );
+$shipping_display = ! empty( $shipping_address['formatted'] ) ? $shipping_address['formatted'] : __( 'Not provided yet.', 'threaddesk' );
+$map_parts = array_filter(
+	array(
+		isset( $address_source['address_1'] ) ? $address_source['address_1'] : '',
+		isset( $address_source['address_2'] ) ? $address_source['address_2'] : '',
+		isset( $address_source['city'] ) ? $address_source['city'] : '',
+		isset( $address_source['state'] ) ? $address_source['state'] : '',
+		isset( $address_source['postcode'] ) ? $address_source['postcode'] : '',
+		isset( $address_source['country'] ) ? $address_source['country'] : '',
+	)
+);
+$map_query = trim( implode( ', ', $map_parts ) );
+$formatted_address = ! empty( $address_source['formatted'] ) ? wp_strip_all_tags( $address_source['formatted'] ) : '';
+if ( '' === $map_query && $formatted_address ) {
+	$map_query = trim( preg_replace( '/\s+/', ' ', str_replace( array( "\r\n", "\r", "\n" ), ', ', $formatted_address ) ) );
+}
+$map_url   = $map_query ? sprintf( 'https://www.google.com/maps?q=%s&output=embed', rawurlencode( $map_query ) ) : '';
+$profile_name = '';
+if ( $user ) {
+	$profile_name = trim( $user->first_name . ' ' . $user->last_name );
+	if ( '' === $profile_name ) {
+		$profile_name = $user->display_name;
+	}
+}
+$profile_name = $profile_name ? $profile_name : $client_name;
+$profile_username = $user ? $user->user_login : __( 'Username', 'threaddesk' );
 
 $nav_base = trailingslashit( wc_get_account_endpoint_url( 'thread-desk' ) );
 
@@ -35,18 +65,29 @@ $format_price = function ( $amount ) use ( $currency ) {
 		<div class="threaddesk__content-inner">
 			<div class="threaddesk__main">
 				<div class="threaddesk__header" style="background-image: url('<?php echo esc_url( $cover ); ?>');">
+					<?php if ( $map_url ) : ?>
+						<div class="threaddesk__header-map" aria-hidden="true">
+							<iframe
+								title="<?php echo esc_attr__( 'Shipping address map', 'threaddesk' ); ?>"
+								src="<?php echo esc_url( $map_url ); ?>"
+								loading="lazy"
+								referrerpolicy="no-referrer-when-downgrade"></iframe>
+						</div>
+					<?php endif; ?>
 					<form class="threaddesk__profile" method="post" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" enctype="multipart/form-data">
 						<input type="hidden" name="action" value="tta_threaddesk_avatar_upload" />
 						<?php wp_nonce_field( 'tta_threaddesk_avatar_upload' ); ?>
-						<label class="threaddesk__avatar<?php echo $avatar_url ? ' has-image' : ''; ?>" for="threaddesk_avatar">
-							<img src="<?php echo esc_url( $avatar_url ); ?>" alt="<?php echo esc_attr__( 'Company avatar', 'threaddesk' ); ?>" />
-							<span class="threaddesk__avatar-overlay"><?php echo esc_html__( 'Upload', 'threaddesk' ); ?></span>
-						</label>
-						<input class="threaddesk__avatar-input" id="threaddesk_avatar" name="threaddesk_avatar" type="file" accept="image/*" onchange="this.form.submit();" />
-						<div class="threaddesk__profile-text">
-							<h2><?php echo esc_html( $client_name ); ?></h2>
-							<p><?php echo esc_html( $user ? $user->user_login : __( 'Username', 'threaddesk' ) ); ?></p>
+						<div class="threaddesk__profile-details">
+							<label class="threaddesk__avatar<?php echo $avatar_url ? ' has-image' : ''; ?>" for="threaddesk_avatar">
+								<img src="<?php echo esc_url( $avatar_url ); ?>" alt="<?php echo esc_attr__( 'Company avatar', 'threaddesk' ); ?>" />
+								<span class="threaddesk__avatar-overlay"><?php echo esc_html__( 'Upload', 'threaddesk' ); ?></span>
+							</label>
+							<div class="threaddesk__profile-text">
+								<h2><?php echo esc_html( $profile_name ); ?></h2>
+								<p><?php echo esc_html( $profile_username ); ?></p>
+							</div>
 						</div>
+						<input class="threaddesk__avatar-input" id="threaddesk_avatar" name="threaddesk_avatar" type="file" accept="image/*" onchange="this.form.submit();" />
 					</form>
 				</div>
 
@@ -121,14 +162,14 @@ $format_price = function ( $amount ) use ( $currency ) {
 						<h4><?php echo esc_html__( 'Billing Details', 'threaddesk' ); ?></h4>
 						<a href="<?php echo esc_url( $context['account_links']['edit_billing'] ); ?>"><?php echo esc_html__( 'Edit', 'threaddesk' ); ?></a>
 					</div>
-					<p><?php echo wp_kses_post( nl2br( $context['billing_address'] ) ); ?></p>
+					<p><?php echo wp_kses_post( nl2br( $billing_display ) ); ?></p>
 				</div>
 				<div class="threaddesk__card">
 					<div class="threaddesk__card-header">
 						<h4><?php echo esc_html__( 'Shipping Details', 'threaddesk' ); ?></h4>
 						<a href="<?php echo esc_url( $context['account_links']['edit_shipping'] ); ?>"><?php echo esc_html__( 'Edit', 'threaddesk' ); ?></a>
 					</div>
-					<p><?php echo wp_kses_post( nl2br( $context['shipping_address'] ) ); ?></p>
+					<p><?php echo wp_kses_post( nl2br( $shipping_display ) ); ?></p>
 				</div>
 				<div class="threaddesk__card">
 					<div class="threaddesk__card-header">
